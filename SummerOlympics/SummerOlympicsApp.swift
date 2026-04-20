6dd36ab69cbd445d36df62829420676befc7d53d
TrunkMstr <baluga.aguilar323@gmail.com>
2026-04-20 14:49:15 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by AGUILAR, DANIEL on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
